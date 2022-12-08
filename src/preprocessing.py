b38c323be7a23370ab233de44977115d2bd60f27
Kenneth Liou <kennethliou01@gmail.com>
2022-12-07 22:38:46 -0500

    float converstion for preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -13,12 +13,14 @@ def get_data_MNIST(subset):
     #Grayscale values are normalized between 0 and 1
     #labels are converted into one hot tensors
     if subset == "train":
+        train_image = tf.cast(train_image, tf.float32)
         train_image = tf.expand_dims(train_image, axis=-1)
         train_image = tf.image.resize(train_image, [32,32])
         image = train_image/255
         label = tf.one_hot(train_label, depth)
 
     if subset == "test":
+        test_image = tf.cast(test_image, tf.float32)
         test_image = tf.expand_dims(test_image, axis=-1)
         test_image = tf.image.resize(test_image, [32,32])
         image = test_image/255
@@ -36,6 +38,7 @@ def get_data_CIFAR(subset, return_one_hot = True):
     # RGB values are normalized between 0 and 1
     # labels are converted into one hot tensors
     if subset == "train":
+        train_image = tf.cast(train_image, tf.float32)
         #train_image = tf.expand_dims(train_image, axis=-1)
         #train_image = tf.image.resize(train_image, [32,32,3])
         image = train_image/255
@@ -46,6 +49,7 @@ def get_data_CIFAR(subset, return_one_hot = True):
 
 
     if subset == "test":
+        test_image = tf.cast(test_image, tf.float32)
         # test_image = tf.expand_dims(test_image, axis=-1)
         # test_image = tf.image.resize(test_image, [32,32,3])
         image = test_image/255
