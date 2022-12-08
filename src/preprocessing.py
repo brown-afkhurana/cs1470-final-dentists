845c4b3c7b13b3e744b674b05d7647afd677aa49
Kenneth Liou <kennethliou01@gmail.com>
2022-12-07 22:08:33 -0500

    cleaned up preprocessing mnist

---
+++ src/preprocessing.py
@@ -0,0 +1,50 @@
+import gzip
+import tensorflow as tf
+
+import numpy as np
+
+def get_data_MNIST(subset):
+    (train_image, train_label), (test_image, test_label) = tf.keras.datasets.mnist.load_data()
+    #60,000 training images and 10,000 test images
+
+    depth = 10
+
+    #image resized to 32x32
+    #Grayscale values are normalized between 0 and 1
+    #labels are converted into one hot tensors
+    if subset == "train":
+        train_image = tf.expand_dims(train_image, axis=-1)
+        train_image = tf.image.resize(train_image, [32,32])
+        image = train_image/255
+        label = tf.one_hot(train_label, depth)
+
+    if subset == "test":
+        test_image = tf.expand_dims(test_image, axis=-1)
+        test_image = tf.image.resize(test_image, [32,32])
+        image = test_image/255
+        label = tf.one_hot(test_label, depth)
+
+    return image, label
+
+def get_data_CIFAR(subset):
+    (train_image, train_label), (test_image, test_label) = tf.keras.datasets.cifar10.load_data()
+    #50,000 training images and 10,000 test images
+
+    depth = 10
+
+    #image resizing is unecessary, so it is commented out
+    #RGB values are normalized between 0 and 1
+    #labels are converted into one hot tensors
+    if subset == "train":
+        #train_image = tf.expand_dims(train_image, axis=-1)
+        #train_image = tf.image.resize(train_image, [32,32,3])
+        image = train_image/255
+        label = tf.one_hot(train_label, depth)
+
+    if subset == "test":
+        #test_image = tf.expand_dims(test_image, axis=-1)
+        #test_image = tf.image.resize(test_image, [32,32,3])
+        image = test_image/255
+        label = tf.one_hot(test_label, depth)
+
+    return image, label
