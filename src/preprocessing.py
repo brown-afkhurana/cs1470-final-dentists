64005d3b310d04c99d1fda52fc9a366c054fa7d7
Kenneth Liou <kennethliou01@gmail.com>
2022-12-10 17:14:29 -0500

    cifar10h data

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -65,3 +65,35 @@ def get_data_CIFAR(subset, return_one_hot = True):
             label = test_label
 
     return image, label
+
+def get_data_CIFAR10H(return_one_hot = True):
+    #cifar10h-probs.npy should be stored in the data package
+    
+    (train_image, train_label), (test_image, test_label) = tf.keras.datasets.cifar10.load_data()
+    #10,000 images from the test set
+
+    depth = 10
+
+    # image resizing is unecessary, so it is commented out
+    # RGB values are normalized between 0 and 1
+    # labels are converted into one hot tensors
+    test_image = tf.cast(test_image, tf.float32)
+    # test_image = tf.expand_dims(test_image, axis=-1)
+    # test_image = tf.image.resize(test_image, [32,32,3])
+    image = test_image/255
+
+    CIFAR10H_file_path = '../data/cifar10h-probs.npy'
+    probability_labels = np.load(CIFAR10H_file_path)
+
+    # convert the 10000x10 array into a 10000 element list of labels
+    for probability_distribution in probability_labels:
+        image_label = probability_distribution.index(max(probability_distribution))
+        probability_distribution = image_label
+
+    if return_one_hot:
+        label = tf.one_hot(probability_labels, depth)
+    else:
+        label = probability_labels
+        
+
+    return image, label
